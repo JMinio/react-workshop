c21aa7557c1d607a234c6b2bc116639a807f4a15
JMinio <juli.mno18@gmail.com>
2023-05-04 20:11:26 -0300

    ItemDetail & ItemJs

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './styles.css';
 import NavBar from "./Components/NavBar/NavBar"
 import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
+import { BrowserRouter, Routes, Route  } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <ItemListContainer msg="Bienvenidos al Himalaya" desc="Welcome to my store."/>
-    </div>
+    <BrowserRouter>
+      <NavBar/>
+      <Routes>
+         <Route exact path="/" element={<ItemListContainer/>}/>
+         <Route exact path="/category/:categoryId" element={<ItemListContainer/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
